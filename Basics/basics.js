e320f2a1a22b45bedb728ff67663a7e305f989cc
Raihan Munim <raihan.srizon@gmail.com>
2022-07-05 23:21:24 +0600

    updated

--- Basics/basics.js
+++ Basics/basics.js
@@ -3,8 +3,18 @@ console.log("Hello World");
 console.log("Raihan Munim");
 
 // variables
-name = "Raihan";
-age = 26;
+var name = "Raihan";
+var age = 26;
 
 console.log(name, age);
 
+// sentences
+sentence1 = "Hello World, I am " + name + " and I am " + age + " years old.";
+console.log(sentence1);
+
+// operators
+Eating = prompt("What do you want to eat: ")
+food = 20
+tipPerc = 0.2
+tipAmount = food * tipPerc
+console.log("You should tip $" + tipAmount + " for your meal.");
