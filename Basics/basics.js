6ebdff52586bb3263431387216d8d0f3cfe1953f
Raihan Munim <raihan.srizon@gmail.com>
2022-07-07 22:53:16 +0600

    updated

--- Basics/basics.js
+++ Basics/basics.js
@@ -1,31 +1,31 @@
 // Commented out to avoid cluttering the console
 
 // Console log
-// console.log("Hello World");
-// console.log("Raihan Munim");
+console.log("Binge JavaScript...");
 
 // variables
-// var name = "Raihan";
-// var age = 26;
+var name = "Raihan";
+var age = 26;
 
 // console.log(name, age);
 
 // sentences
-// sentence1 = "Hello World, I am " + name + " and I am " + age + " years old.";
+sentence1 = "Hello World, I am " + name + " and I am " + age + " years old.";
 // console.log(sentence1);
 
 // operators
 const prompt = require("prompt-sync")();
 
 // wrap in number to avoid typecasting
-// var food = Number(prompt('How much was the food: '));
-// var tipPercent = Number(prompt('What is the tip percentage: ')) / 100;
-// tipAmount = food * tipPercent
-// total = food + tipAmount
+var food = Number(prompt('How much was the food: '));
+var tipPercent = Number(prompt('What is the tip percentage: ')) / 100;
+tipAmount = food * tipPercent
+total = food + tipAmount
 
 // console.log("The tip amount is $" + tipAmount);
 // console.log("The total is $" + total);
 
 // data types
 // Ctrl + I for emoji 
+
 // Time: 24:18
