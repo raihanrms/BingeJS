c1eccdef5160acde62dd04bdbdb43d6736979c88
Raihan Munim <raihan.srizon@gmail.com>
2022-08-01 03:07:03 +0600

    updated

--- projects/superHero/script.js
+++ projects/superHero/script.js
@@ -1,3 +1,9 @@
-// alert the user
-// Super Hero API website is down, will code this later. 
-// Youtube time: 6:34:19"
+// url: https://superheroapi.com/api.php/2559147114220333/245
+
+const getSuperHero = () => {
+    fetch('https://superheroapi.com/api.php/2559147114220333/245')
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+getSuperHero()
