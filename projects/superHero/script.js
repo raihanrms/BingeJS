922d735ec0026e9f26d6fa270cd3f2bca3f36049
Raihan Munim <raihan.srizon@gmail.com>
2022-08-01 23:58:02 +0600

    updated

--- projects/superHero/script.js
+++ projects/superHero/script.js
@@ -1,10 +1,19 @@
 // url: https://superheroapi.com/api.php/2559147114220333/245
 
-const getSuperHero = () => {
-    fetch('https://superheroapi.com/api/2559147114220333/245')
+// api cant be fetched, getting ssl error. Moving on.
+
+const superheroToken = '2559147114220333';
+const baseUrl = `https://superheroapi.com/api.php/${superheroToken}/`
+
+const getSuperHero = (id, name) => {
+    fetch(`${baseUrl}/${id}`)
     .then(response => response.json())
-    .then(json => console.log(json))
+    .then(json => {
+        console.log(json)
+        document.querySelector('body').innerHTML += `<img src="${json.image.url}" height=200 width=200/>`
+    })
 }
 
-getSuperHero()
-// test
+getSuperHero(20)
+
+const img = 'https://superherodb.com/pictures2/portraits/10/100/1390.jpg'
