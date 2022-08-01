0bce29884ec0fa71b754ab3386eeebb0ab0fd3e9
Raihan Munim <raihan.srizon@gmail.com>
2022-08-01 22:41:39 +0600

    updated

--- projects/superHero/script.js
+++ projects/superHero/script.js
@@ -1,7 +1,7 @@
 // url: https://superheroapi.com/api.php/2559147114220333/245
 
 const getSuperHero = () => {
-    fetch('https://superheroapi.com/api.php/2559147114220333/245')
+    fetch('https://superheroapi.com/api/2559147114220333/245')
     .then(response => response.json())
     .then(json => console.log(json))
 }
