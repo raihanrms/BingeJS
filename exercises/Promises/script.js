4ec6f6bf3c408d1fb4c37229a1ecdbb2deae5326
Raihan Munim <raihan.srizon@gmail.com>
2022-08-03 00:41:40 +0600

    updated

--- exercises/Promises/script.js
+++ exercises/Promises/script.js
@@ -1,13 +1,30 @@
-(function () {
-    var old = console.log;
-    var logger = document.getElementById('log');
-    console.log = function () {
-      for (var i = 0; i < arguments.length; i++) {
-        if (typeof arguments[i] == 'object') {
-            logger.innerHTML += (JSON && JSON.stringify ? JSON.stringify(arguments[i], undefined, 2) : arguments[i]) + '<br />';
-        } else {
-            logger.innerHTML += arguments[i] + '<br />';
-        }
+function logThis(message) {
+    // if we pass an Error object, message.stack will have all the details, otherwise give us a string
+    if (typeof message === 'object') {
+      message = message.stack || objToString(message);
+    } 
+    console.log(message);
+  
+    // create the message line with current time
+    var today = new Date();
+    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+    var dateTime = date + ' ' + time + ' ';
+  
+    //insert line
+    document.getElementById('logger')
+    .insertAdjacentHTML('afterbegin', dateTime + message + '<br>');
+  }
+  
+  function objToString(obj) {
+    var str = 'Object: ';
+    for (var p in obj) {
+      if (obj.hasOwnProperty(p)) {
+        str += p + '::' + obj[p] + ',\n';
       }
     }
-})();
+    return str;
+  }
+
+const waitingSoup = () => logThis('soup')  
+setTimeout(waitingSoup, 2000)
