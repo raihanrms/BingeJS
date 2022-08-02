244ed5ed4181f5029d2d6fede740cc4d01014344
Raihan Munim <raihan.srizon@gmail.com>
2022-08-03 01:09:37 +0600

    updated

--- exercises/Promises/script.js
+++ exercises/Promises/script.js
@@ -1,30 +1,15 @@
-function logThis(message) {
-    // if we pass an Error object, message.stack will have all the details, otherwise give us a string
-    if (typeof message === 'object') {
-      message = message.stack || objToString(message);
-    } 
-    console.log(message);
-  
-    // create the message line with current time
-    var today = new Date();
-    var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    var time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
-    var dateTime = date + ' ' + time + ' ';
-  
-    //insert line
-    document.getElementById('logger')
-    .insertAdjacentHTML('afterbegin', dateTime + message + '<br>');
-  }
-  
-  function objToString(obj) {
-    var str = 'Object: ';
-    for (var p in obj) {
-      if (obj.hasOwnProperty(p)) {
-        str += p + '::' + obj[p] + ',\n';
-      }
-    }
-    return str;
-  }
+const waitingSoup = () => console.log('soup')
 
-const waitingSoup = () => logThis('soup')  
-setTimeout(waitingSoup, 2000)
+const promiseOne = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // will always pick a random from the index out of the two items
+        isReady = [true, false][Math.floor(Math.random() * 2)];
+        isReady ? resolve('✅ Soup is ready 🥣'): reject('❌ No Soup today 🥔')
+    }, 2000)
+})
+
+// .then is a function that takes a callback function
+console.log(promiseOne
+    .then(success => console.log({success}))
+    .catch(error => console.log({error}))
+)
