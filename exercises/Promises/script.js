a0b33d36833e4feae38838e6c757c1a9d4731f14
Raihan Munim <raihan.srizon@gmail.com>
2022-08-03 02:26:02 +0600

    updated

--- exercises/Promises/script.js
+++ exercises/Promises/script.js
@@ -1,4 +1,6 @@
-const waitingSoup = () => console.log('soup')
+console.log('Async and Await!')
+
+// const waitingSoup = () => console.log('soup')
 
 const promiseOne = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -9,7 +11,57 @@ const promiseOne = new Promise((resolve, reject) => {
 })
 
 // .then is a function that takes a callback function
-console.log(promiseOne
-    .then(success => console.log({success}))
-    .catch(error => console.log({error}))
-)
+// console.log(promiseOne
+//     .then(success => console.log({success}))
+//     .catch(error => console.log({error}))
+// )
+
+// fetch is a promise that returns a response
+// console.log('fetch:', fetch('https://dog.ceo/api/breeds/image/random')
+// .then(response => response.json())
+// // access to the actual data
+// .then(data => console.log(data))
+// )
+
+// Async and Await
+// Rules for using async and await:
+// 1. Must be in a function, 2. Use the keyword 'async' 3. Use the word 'await'
+const getDog = async () => {
+    // await waits for the promise to resolve
+    const url = 'https://dog.ceo/api/breeds/image/random'
+    const response = await fetch(url) 
+    const data = await response.json()
+    console.log(data)
+}
+getDog()
+
+// Resolved - pay the waiter
+// Rejected - leave the store
+
+// get soup using async and await
+const getSoupAsyncAwait = async () => {
+    
+    // object that represents the promise
+    const data = {rating: 0, tip: 0, pay: 0, review: 0}
+    
+    try {
+    const soup = await promiseOne
+    console.log(soup)
+    data.rating = 5
+    data.tip = .2
+    data.pay = 10
+    data.review = 5
+    return data
+
+} catch(error) {
+    console.log('error:', error)
+    data.rating = 1
+    data.tip = 0
+    data.pay = 0
+    data.review = 1
+    return data
+    }
+}
+
+// to get this function we have to console log the value
+getSoupAsyncAwait().then(value => console.log(value))
