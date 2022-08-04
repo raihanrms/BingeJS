4b5dc738b578b340532061690ce62be6b64149a3
Raihan Munim <raihan.srizon@gmail.com>
2022-08-04 23:48:36 +0600

    updated

--- exercises/api-testing-weather/script.js
+++ exercises/api-testing-weather/script.js
@@ -6,7 +6,7 @@ const options = {
 	}
 };
 
-fetch('https://weatherapi-com.p.rapidapi.com/future.json?q=London&dt=2022-12-25', options)
+fetch('https://weatherapi-com.p.rapidapi.com/future.json?q=Dhaka&dt=2022-12-25', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
