fb2223dfeb22dcdedb812c2bb26d409b9f6aeaca
Raihan Munim <raihan.srizon@gmail.com>
2022-08-07 23:55:58 +0600

    updated

--- projects/weather-app/script.js
+++ projects/weather-app/script.js
@@ -1,47 +1,39 @@
 /**
  * Weather App
- * TODO: Complete getWeatherData() to return json response Promise
- * TODO: Complete searchCity() to get user input and get data using getWeatherData()
- * TODO: Complete showWeatherData() to set the data in the the html file from response
+ * Completed getWeatherData() to return json response Promise
+ * Completed searchCity() to get user input and get data using getWeatherData()
+ * Completed showWeatherData() to set the data in the the html file from response
  */
 
 // API_KEY for maps api
-API_KEY = 'a8e71c9932b20c4ceb0aed183e6a83bb'
+let API_KEY = 'a8e71c9932b20c4ceb0aed183e6a83bb';
 
-/**
- * Retrieve weather data from openweathermap
- * HINT: Use fetch()
- * HINT: URL should look like this: 
- * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
- */
-getWeatherData = () => {
-  //HINT: Use template literals to create a url with input and an API key
-  //CODE GOES HERE
-
-
-  // fetch('https://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=a8e71c9932b20c4ceb0aed183e6a83bb')
-	// .then(response => response.json())
-	// .then(data => console.log(data.weather[0].main))
-	// .catch(err => console.error(err));
-
+getWeatherData = async (city) => {
+  const URL = 'https://api.openweathermap.org/data/2.5/weather';
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise = fetch(FULL_URL);
+  const response = await weatherPromise;
+  return await response.json();
 }
 
-/**
- * Retrieve city input and get the weather data
- * HINT: Use the promise returned from getWeatherData()
- */
-searchCity = () => {
+const searchCity = async () => {
   const city = document.getElementById('city-input').value;
-  console.log(city)
-
+  getWeatherData(city).then((res) => {
+    showWeatherData(res);
+  }).catch((error) => {
+    console.log(error);
+    console.log("Something happend");
+  })
 }
 
-/**
- * Show the weather data in HTML
- * HINT: make sure to console log the weatherData to see how the data looks like
- */
-showWeatherData = (weatherData) => {
-  //CODE GOES HERE
+const showWeatherData = (weatherData) => {
+  console.log(weatherData.main.temp)
+  document.getElementById('temp').innerText = weatherData.main.temp
+  document.getElementById('temp').innerText = weatherData.main.temp
+  document.getElementById('temp').innerText = weatherData.main.temp
+  document.getElementById('temp').innerText = weatherData.main.temp
+  document.getElementById('temp').innerText = weatherData.main.temp
+
   
 }
 
