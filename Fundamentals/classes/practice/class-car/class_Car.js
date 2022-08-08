b3e09ebe70d6f387efec6889451e56e1fcbb2952
Raihan Munim <raihan.srizon@gmail.com>
2022-08-08 19:04:22 +0600

    updated

---
+++ Fundamentals/classes/practice/class-car/class_Car.js
@@ -0,0 +1,16 @@
+// classes
+// methods
+// properties
+
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+nums.push(11);
+
+console.log(nums);
+
+class Car {
+    constructor(name, color, speed){
+        this.name = name
+        this.color = color
+        this.speed = speed
+    }
+}
